7e460eec5f93c1ae0ea4f379d6f315a6b1b5f32d
ChutimunTorntranon <ChutimunTorntranon@gamil.com>
2022-10-17 15:47:27 +0700

    edit more more

--- src/feature/whitelist/WhitelistContainer.jsx
+++ src/feature/whitelist/WhitelistContainer.jsx
@@ -1,83 +1,20 @@
+import WhitelisItem from './WhitelisItem';
+
 function WhitelistContainer() {
 	return (
 		<>
-			<div className='w-screen container mx-auto'>
+			<div className='mx-[5rem]'>
 				<div className='text-[2rem] mt-[1rem]'>
 					<h1 className='my-auto'>Whitelists</h1>
 				</div>
-				<div className='grid grid-flow-col gap-3'>
-					<div className='mt-[2rem]'>
-						<div className='w-[26rem] h-[16rem] flex'>
-							<div>
-								<img
-									className='w-[20rem] h-[16rem] rounded-tl-3xl rounded-bl-3xl hover:shadow-xl'
-									src='https://img.freepik.com/free-photo/pattaya-city_1203-2712.jpg?w=2000&t=st=1665733615~exp=1665734215~hmac=2526010c52349328b0727832f7d88a57c756a91e55661fa4e349541616d8a23b'
-								></img>
-							</div>
-							<div className='flex-colum'>
-								<img
-									className='w-[10rem] h-[8rem] rounded-tr-3xl hover:shadow-xl'
-									src='https://img.freepik.com/free-photo/relax-couple-lay-down-beach-chiar-with-sea-wave-man-woman-have-vacation-sea-nature-concept_1150-13745.jpg?w=2000&t=st=1665733679~exp=1665734279~hmac=bcf104ce3e032a4556781e8d23de6f15a4b4ef2a6c85bf7a563bceaad0e87a4e'
-								></img>
-
-								<img
-									className='w-[10rem] h-[8rem] rounded-br-3xl hover:shadow-xl'
-									src='https://img.freepik.com/free-photo/wooden-bridge-koh-nangyuan-island-surat-thani-thailand_335224-1082.jpg?w=2000&t=st=1665733695~exp=1665734295~hmac=1e872fa6e5e8d628ab9670fcfc5f4ee4e8ccea4b74ae6a7ddcad79970c3e0984'
-								></img>
-							</div>
-						</div>
-						<div className='text-[1.5rem]'>
-							<h1>Test</h1>
-						</div>
-					</div>
-					<div className='mt-[2rem]'>
-						<div className='w-[26rem] h-[16rem] flex'>
-							<div>
-								<img
-									className='w-[20rem] h-[16rem] rounded-tl-3xl rounded-bl-3xl hover:shadow-xl'
-									src='https://img.freepik.com/free-photo/pattaya-city_1203-2712.jpg?w=2000&t=st=1665733615~exp=1665734215~hmac=2526010c52349328b0727832f7d88a57c756a91e55661fa4e349541616d8a23b'
-								></img>
-							</div>
-							<div className='flex-colum'>
-								<img
-									className='w-[10rem] h-[8rem] rounded-tr-3xl '
-									src='https://img.freepik.com/free-photo/relax-couple-lay-down-beach-chiar-with-sea-wave-man-woman-have-vacation-sea-nature-concept_1150-13745.jpg?w=2000&t=st=1665733679~exp=1665734279~hmac=bcf104ce3e032a4556781e8d23de6f15a4b4ef2a6c85bf7a563bceaad0e87a4e'
-								></img>
-
-								<img
-									className='w-[10rem] h-[8rem] rounded-br-3xl'
-									src='https://img.freepik.com/free-photo/wooden-bridge-koh-nangyuan-island-surat-thani-thailand_335224-1082.jpg?w=2000&t=st=1665733695~exp=1665734295~hmac=1e872fa6e5e8d628ab9670fcfc5f4ee4e8ccea4b74ae6a7ddcad79970c3e0984'
-								></img>
-							</div>
-						</div>
-						<div className='text-[1.5rem]'>
-							<h1>Test</h1>
-						</div>
-					</div>
-					<div className='mt-[2rem]'>
-						<div className='w-[26rem] h-[16rem] flex'>
-							<div>
-								<img
-									className='w-[20rem] h-[16rem] rounded-tl-3xl rounded-bl-3xl'
-									src='https://img.freepik.com/free-photo/pattaya-city_1203-2712.jpg?w=2000&t=st=1665733615~exp=1665734215~hmac=2526010c52349328b0727832f7d88a57c756a91e55661fa4e349541616d8a23b'
-								></img>
-							</div>
-							<div className='flex-colum'>
-								<img
-									className='w-[10rem] h-[8rem] rounded-tr-3xl'
-									src='https://img.freepik.com/free-photo/relax-couple-lay-down-beach-chiar-with-sea-wave-man-woman-have-vacation-sea-nature-concept_1150-13745.jpg?w=2000&t=st=1665733679~exp=1665734279~hmac=bcf104ce3e032a4556781e8d23de6f15a4b4ef2a6c85bf7a563bceaad0e87a4e'
-								></img>
-
-								<img
-									className='w-[10rem] h-[8rem] rounded-br-3xl'
-									src='https://img.freepik.com/free-photo/wooden-bridge-koh-nangyuan-island-surat-thani-thailand_335224-1082.jpg?w=2000&t=st=1665733695~exp=1665734295~hmac=1e872fa6e5e8d628ab9670fcfc5f4ee4e8ccea4b74ae6a7ddcad79970c3e0984'
-								></img>
-							</div>
-						</div>
-						<div className='text-[1.5rem]'>
-							<h1>Test</h1>
-						</div>
-					</div>
+				<div className='grid grid-cols-3 content-center'>
+					<WhitelisItem />
+					<WhitelisItem />
+					<WhitelisItem />
+					<WhitelisItem />
+					<WhitelisItem />
+					<WhitelisItem />
+					<WhitelisItem />
 				</div>
 			</div>
 		</>
@@ -85,43 +22,3 @@ function WhitelistContainer() {
 }
 
 export default WhitelistContainer;
-
-{
-	/* <div className='w-[26rem] h-[16rem] flex'>
-						<img
-							className='w-[15rem] h-[16rem] rounded-tl-3xl rounded-bl-3xl'
-							src='https://img.freepik.com/free-photo/aerial-view-railay-beach-krabi-thailand_335224-1180.jpg?w=2000&t=st=1665733795~exp=1665734395~hmac=c6c17ba8fd96b2a6e09030518e944d2ded2bec4b91d56f2b7f30fc9f820cfb91'
-						></img>
-						<div className='flex-colum'>
-							<img
-								className='w-[10rem] h-[8rem] rounded-tr-3xl'
-								src='https://img.freepik.com/free-photo/long-boat-tourist-maya-bay-phi-phi-island-photo-taken-december-1-2016-krabi-thailand_335224-899.jpg?w=2000&t=st=1665733833~exp=1665734433~hmac=ddfe08716d5b93e3b79e4956f89955312259dfa959b3286cb38a972a5361b64c'
-							></img>
-
-							<img
-								className='w-[10rem] h-[8rem] rounded-br-3xl'
-								src='https://img.freepik.com/free-photo/long-tail-boat-turquoise-crystal-clear-sea-water-with-limestone-cliff-mountain-phra-nang-beach-krabi-thailand_554837-867.jpg?w=2000&t=st=1665733886~exp=1665734486~hmac=7d731706e92753a931a77368452defa0152f3dff4887d815c87a9198b015388e'
-							></img>
-						</div>
-					</div> */
-}
-
-{
-	/* <div className='w-[26rem] h-[16rem] flex'>
-						<img
-							className='w-[15rem] h-[16rem] rounded-tl-3xl rounded-bl-3xl'
-							src='https://img.freepik.com/free-photo/buddha-statue-wat-mahathat-temple-precinct-sukhothai-historical-park_335224-911.jpg?w=2000&t=st=1665734332~exp=1665734932~hmac=da5f2132229fec06b30ecbee1f6847c06fbe6feef7f451f5cf793a6d01bb2def'
-						></img>
-						<div className='flex-colum'>
-							<img
-								className='w-[10rem] h-[8rem] rounded-tr-3xl'
-								src='https://img.freepik.com/premium-photo/beautiful-temple-wat-chaiwatthanaram-area-ayutthaya-historical-park-ayutthaya-thailand_35977-3240.jpg?w=2000'
-							></img>
-
-							<img
-								className='w-[10rem] h-[8rem] rounded-br-3xl'
-								src='https://img.freepik.com/premium-photo/foreign-tourists-elephant-ride-visit-ayutthaya-there-are-ruins-temple-ayutthaya-period-concept-is-travel-temple_39733-436.jpg?w=1800'
-							></img>
-						</div>
-					</div> */
-}
