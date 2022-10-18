b46b2d3de58586d501b88340a63463a4363dc535
ChutimunTorntranon <ChutimunTorntranon@gamil.com>
2022-10-18 17:45:00 +0700

    success reserve

--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import CalendarModal from '../components/Modal/CalendarModal';
 import EmailLoginModal from '../components/Modal/EmailLoginModal';
 import LoginRegisterModal from '../components/Modal/LoginRegisterModal';
+import ReserveComponent from '../components/ReserveComponent';
 import AuthLayout from '../layout/auth/AuthLayout';
 import HomeLayout from '../layout/auth/HomeLayout';
 import AccountPage from '../pages/AccountPage';
@@ -9,23 +11,27 @@ import HomePage from '../pages/HomePage';
 import WhitelistPage from '../pages/WhitelistPage';
 
 function Router() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/account" element={<AccountPage />}></Route>
-          <Route path="/whitelist" element={<WhitelistPage />}></Route>
-          <Route path="/login" element={<LoginRegisterModal />}></Route>
-          <Route path="/loginEmail" element={<EmailLoginModal />}></Route>
-          <Route
-            path="/confirmPayment"
-            element={<ConfirmPaymentPage />}
-          ></Route>
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<AuthLayout />}>
+					<Route path='/' element={<HomePage />}></Route>
+					<Route path='/account' element={<AccountPage />}></Route>
+					<Route path='/whitelist' element={<WhitelistPage />}></Route>
+					<Route path='/login' element={<LoginRegisterModal />}></Route>
+					<Route path='/loginEmail' element={<EmailLoginModal />}></Route>
+					<Route path='/reserve' element={<ReserveComponent />}></Route>
+
+					<Route path='/calendar' element={<CalendarModal />}></Route>
+
+					<Route
+						path='/confirmPayment'
+						element={<ConfirmPaymentPage />}
+					></Route>
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default Router;
