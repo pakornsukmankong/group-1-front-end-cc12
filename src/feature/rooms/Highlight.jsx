12f3df866b933acb524f4042db96ad6adc743b44
pakornsukmankong <floksong.001@gmail.com>
2022-10-18 12:22:50 +0700

    singlePage-responsive

---
+++ src/feature/rooms/Highlight.jsx
@@ -0,0 +1,18 @@
+function Highlight() {
+  return (
+    <div className="flex items-center gap-4">
+      <svg viewBox="0 0 32 32" className="block h-6 w-6">
+        <path d="m24.3334 1.66675c1.0543745 0 1.9181663.81587127 1.9945143 1.85073677l.0054857.14926323-.00065 24.666 3.00065.00075v2h-26.66665v-2l3-.00075v-24.666c0-1.05436681.81587301-1.91816558 1.850737-1.99451429l.149263-.00548571zm-4.00065 2h-12.666l-.00075 24.66625 12.66675-.00025zm4.00065 0h-2.00065v24.666l2.00025.00025zm-7.0001 11.00002c.7363778 0 1.33333.5969522 1.33333 1.33333s-.5969522 1.33333-1.33333 1.33333-1.33333-.5969522-1.33333-1.33333.5969522-1.33333 1.33333-1.33333z" />
+      </svg>
+      {/* <img src="" className="w-6 h-6" /> */}
+      <div>
+        <p className="font-medium">Self Check-in</p>
+        <p className="font-thin text-gray-500">
+          You can check in with the doorman.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Highlight
--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -4,19 +4,21 @@ import HomeLayout from '../layout/auth/HomeLayout'
 import AccountPage from '../pages/AccountPage'
 import ConfirmPaymentPage from '../pages/ConfirmPaymentPage'
 import HomePage from '../pages/HomePage'
+import RoomPage from '../pages/RoomPage'
 import WhitelistPage from '../pages/WhitelistPage'
 
 function Router() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomeLayout />}>
+        <Route path="/" element={<AuthLayout />}>
           <Route path="/" element={<HomePage />} />
         </Route>
-        <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<HomeLayout />}>
           <Route path="/account" element={<AccountPage />} />
           <Route path="/whitelist" element={<WhitelistPage />} />
           <Route path="/confirmPayment" element={<ConfirmPaymentPage />} />
+          <Route path="/rooms" element={<RoomPage />} />
         </Route>
       </Routes>
     </>
