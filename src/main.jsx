458276d98b1bc107832e6a84b822afc95a606670
ChutimunTorntranon <ChutimunTorntranon@gamil.com>
2022-10-18 18:05:12 +0700

    Add reserve

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,15 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { store } from './store/index';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+	// <React.StrictMode>
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>
+	// </React.StrictMode>
 );
--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -1,36 +1,36 @@
-
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import EmailLoginModal from '../components/Modal/EmailLoginModal';
 import LoginRegisterModal from '../components/Modal/LoginRegisterModal';
 import RegisterModal from '../components/Modal/RegisterModal';
-import AuthLayout from '../layout/auth/AuthLayout'
-import HomeLayout from '../layout/auth/HomeLayout'
-import AccountPage from '../pages/AccountPage'
-import ConfirmPaymentPage from '../pages/ConfirmPaymentPage'
-import HomePage from '../pages/HomePage'
-import RoomPage from '../pages/RoomPage'
-import WhitelistPage from '../pages/WhitelistPage'
-
+import ReserveComponent from '../components/Modal/ReserveComponent';
+import AuthLayout from '../layout/auth/AuthLayout';
+import HomeLayout from '../layout/auth/HomeLayout';
+import AccountPage from '../pages/AccountPage';
+import ConfirmPaymentPage from '../pages/ConfirmPaymentPage';
+import HomePage from '../pages/HomePage';
+import RoomPage from '../pages/RoomPage';
+import WhitelistPage from '../pages/WhitelistPage';
 
 function Router() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route path="/" element={<HomePage />} />
-        </Route>
-        <Route path="/" element={<HomeLayout />}>
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/whitelist" element={<WhitelistPage />} />
-          <Route path="/confirmPayment" element={<ConfirmPaymentPage />} />
-          <Route path="/rooms" element={<RoomPage />} />
-          <Route path="/login" element={<LoginRegisterModal />}></Route>
-          <Route path="/loginEmail" element={<EmailLoginModal />}></Route>
-          <Route path="/register" element={<RegisterModal />}></Route>
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<AuthLayout />}>
+					<Route path='/' element={<HomePage />} />
+				</Route>
+				<Route path='/' element={<HomeLayout />}>
+					<Route path='/account' element={<AccountPage />} />
+					<Route path='/whitelist' element={<WhitelistPage />} />
+					<Route path='/confirmPayment' element={<ConfirmPaymentPage />} />
+					<Route path='/rooms' element={<RoomPage />} />
+					<Route path='/login' element={<LoginRegisterModal />}></Route>
+					<Route path='/loginEmail' element={<EmailLoginModal />}></Route>
+					<Route path='/register' element={<RegisterModal />}></Route>
+					<Route path='/reserve' element={<ReserveComponent />}></Route>
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default Router;
