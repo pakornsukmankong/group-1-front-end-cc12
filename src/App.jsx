b01cb93ae8bce965715720d99c423c293ddca048
pakornsukmankong <floksong.001@gmail.com>
2022-10-22 19:16:37 +0700

    add loading-toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify'
+import { useSelector } from 'react-redux'
 import { useLoading } from './contexts/LoadingContext'
 import Router from './route/Router'
 import Spinner from './components/Spinner'
@@ -6,12 +8,18 @@ import { useAuth } from './contexts/AuthContext'
 function App() {
   const { loading } = useLoading()
   const { initialLoading } = useAuth()
+  const authSliceLoading = useSelector((state) => state.auth.loading)
 
   if (initialLoading) return <Spinner />
   return (
     <>
-      {loading && <Spinner />}
+      {(loading || authSliceLoading) && <Spinner />}
       <Router />
+      <ToastContainer
+        autoClose={2000}
+        theme="colored"
+        position="bottom-center"
+      />
     </>
   )
 }
---
+++ src/utils/toast.jsx
@@ -0,0 +1,5 @@
+import { toast } from 'react-toastify'
+
+export const toastSuccess = (message) => toast.success(message)
+
+export const toastError = (message) => toast.error(message)
