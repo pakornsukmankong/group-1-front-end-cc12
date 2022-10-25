d797d365efd8a336afa85ba5da01e4100e6d6d99
ChutimunTorntranon <ChutimunTorntranon@gamil.com>
2022-10-25 15:07:20 +0700

    react-slick

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      aspectRatio: {
-        '149125/141668': '149125 / 141668'
-      }
-    }
-  },
-  plugins: []
+	content: [
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+	],
+	theme: {
+		extend: {
+			aspectRatio: {
+				'149125/141668': '149125 / 141668',
+			},
+		},
+	},
+	plugins: [require('flowbite/plugin')],
 };
