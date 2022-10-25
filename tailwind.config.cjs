8160aff060c919915e949024807723e91706d2fe
pakornsukmankong <floksong.001@gmail.com>
2022-10-25 15:17:13 +0700

    fix bug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{js,jsx,ts,tsx}',
-		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-	],
-	theme: {
-		extend: {
-			aspectRatio: {
-				'149125/141668': '149125 / 141668',
-			},
-		},
-	},
-	plugins: [require('flowbite/plugin')],
-};
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      aspectRatio: {
+        '149125/141668': '149125 / 141668',
+      },
+    },
+  },
+}
