88ab0f3ad813e86c032c81c6adc662a232e1ea6a
mayapinya <apinya.th28@gmail.com>
2022-10-27 11:00:35 +0700

    add link

--- src/feature/create-host/PreviewContainer.jsx
+++ src/feature/create-host/PreviewContainer.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getHostPreview, updateHostSavePreview } from '../../api/hostApi';
 import { getHostCreateId, removeHostCreateId } from '../../utils/localStorage';
 import TopMenu from './TopMenu';
