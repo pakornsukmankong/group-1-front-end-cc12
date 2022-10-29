86931d19b63049dc23ec58f237a732737c393c49
pakornsukmankong <floksong.001@gmail.com>
2022-10-30 00:07:10 +0700

    add toast error loginEmail

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,130 +1,130 @@
-import { useEffect, useState, createContext, useContext } from 'react';
-import * as authService from '../api/authApi';
+import { useEffect, useState, createContext, useContext } from 'react'
+import * as authService from '../api/authApi'
 import {
-	getLocalStorage,
-	addLocalStorage,
-	removeLocalStorage,
-} from '../utils/localStorage';
-import { toastSuccess } from '../utils/toast';
+  getLocalStorage,
+  addLocalStorage,
+  removeLocalStorage,
+} from '../utils/localStorage'
+import { toastError, toastSuccess } from '../utils/toast'
 
-const AuthContext = createContext();
+const AuthContext = createContext()
 
 function AuthContextProvider({ children }) {
-	const [user, setUser] = useState(null);
-	const [phoneNumber, setPhoneNumber] = useState('');
-	const [verifyStatus, setVerifyStatus] = useState('');
-	const [initialLoading, setInitialLoading] = useState(true);
-	const [isRegisterEmail, setIsRegisterEmail] = useState('');
+  const [user, setUser] = useState(null)
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [verifyStatus, setVerifyStatus] = useState('')
+  const [initialLoading, setInitialLoading] = useState(true)
+  const [isRegisterEmail, setIsRegisterEmail] = useState('')
 
-	console.log(isRegisterEmail);
+  console.log(isRegisterEmail)
 
-	useEffect(() => {
-		const fetchMe = async () => {
-			try {
-				if (getLocalStorage()) {
-					await getMe();
-				}
-			} catch (err) {
-				console.log(err);
-			} finally {
-				setInitialLoading(false);
-			}
-		};
-		fetchMe();
-	}, []);
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        if (getLocalStorage()) {
+          await getMe()
+        }
+      } catch (err) {
+        console.log(err)
+      } finally {
+        setInitialLoading(false)
+      }
+    }
+    fetchMe()
+  }, [])
 
-	const getMe = async () => {
-		const res = await authService.getMe();
-		setUser(res.data.user);
-		// console.log(res.data.user);
-	};
+  const getMe = async () => {
+    const res = await authService.getMe()
+    setUser(res.data.user)
+    // console.log(res.data.user);
+  }
 
-	const sendOutOtp = async (phoneNumber) => {
-		const res = await authService.sendOtp(phoneNumber);
-		setPhoneNumber(res.data.customerPhoneNumber);
-	};
+  const sendOutOtp = async (phoneNumber) => {
+    const res = await authService.sendOtp(phoneNumber)
+    setPhoneNumber(res.data.customerPhoneNumber)
+  }
 
-	const verifyOtp = async (phoneNumber, code) => {
-		const res = await authService.verifyOtp(phoneNumber, code);
-		addLocalStorage(res.data.token);
-		setUser(res.data.user);
-		setVerifyStatus(res.data.statusOtp);
-	};
+  const verifyOtp = async (phoneNumber, code) => {
+    const res = await authService.verifyOtp(phoneNumber, code)
+    addLocalStorage(res.data.token)
+    setUser(res.data.user)
+    setVerifyStatus(res.data.statusOtp)
+  }
 
-	const loginWithEmail = async (input) => {
-		const res = await authService.loginWithEmail(input);
-		if (res.data.token) {
-			setIsRegisterEmail(res.data.msg);
-			addLocalStorage(res.data.token);
-			getMe();
-			toastSuccess('Login Success');
-		}
+  const loginWithEmail = async (input) => {
+    const res = await authService.loginWithEmail(input)
+    if (res.data.token) {
+      setIsRegisterEmail(res.data.msg)
+      addLocalStorage(res.data.token)
+      getMe()
+      toastSuccess('Login Success')
+    }
 
-		if (res.data.msg === 'notRegister') {
-			setIsRegisterEmail(res.data.msg);
-		}
-	};
+    if (res.data.msg === 'notRegister') {
+      setIsRegisterEmail(res.data.msg)
+      toastError('You are not register')
+    }
+  }
 
-	const register = async (input) => {
-		const res = await authService.register(input);
-		addLocalStorage(res.data.token);
-		getMe();
-	};
+  const register = async (input) => {
+    const res = await authService.register(input)
+    addLocalStorage(res.data.token)
+    getMe()
+  }
 
-	const logout = async () => {
-		removeLocalStorage();
-		setUser(null);
-		setPhoneNumber('');
-		setVerifyStatus('');
-		window.location.assign('/');
-	};
+  const logout = async () => {
+    removeLocalStorage()
+    setUser(null)
+    setPhoneNumber('')
+    setVerifyStatus('')
+    window.location.assign('/')
+  }
 
-	const updateAccountUser = async (data) => {
-		await authService.updateUser(data);
-		getMe();
-		// return res;
-	};
+  const updateAccountUser = async (data) => {
+    await authService.updateUser(data)
+    getMe()
+    // return res;
+  }
 
-	const deleteUser = async () => {
-		try {
-			await authService.deleteUser();
-			removeLocalStorage();
-			window.location.assign('/');
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const deleteUser = async () => {
+    try {
+      await authService.deleteUser()
+      removeLocalStorage()
+      window.location.assign('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-	const closeModalRegisWithEmail = () => {
-		setIsRegisterEmail('');
-	};
+  const closeModalRegisWithEmail = () => {
+    setIsRegisterEmail('')
+  }
 
-	return (
-		<AuthContext.Provider
-			value={{
-				initialLoading,
-				user,
-				loginWithEmail,
-				register,
-				logout,
-				sendOutOtp,
-				verifyOtp,
-				phoneNumber,
-				verifyStatus,
-				updateAccountUser,
-				deleteUser,
-				setVerifyStatus,
-				isRegisterEmail,
-				closeModalRegisWithEmail,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{
+        initialLoading,
+        user,
+        loginWithEmail,
+        register,
+        logout,
+        sendOutOtp,
+        verifyOtp,
+        phoneNumber,
+        verifyStatus,
+        updateAccountUser,
+        deleteUser,
+        setVerifyStatus,
+        isRegisterEmail,
+        closeModalRegisWithEmail,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export const useAuth = () => {
-	return useContext(AuthContext);
-};
+  return useContext(AuthContext)
+}
 
-export default AuthContextProvider;
+export default AuthContextProvider
