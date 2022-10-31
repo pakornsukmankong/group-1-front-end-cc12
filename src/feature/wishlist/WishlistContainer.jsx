fd6c3be7c1ae5fa640e0f774fe700a8f99dbd18a
pakornsukmankong <floksong.001@gmail.com>
2022-10-31 16:40:39 +0700

    fix bug

--- src/feature/wishlist/WishlistContainer.jsx
+++ src/feature/wishlist/WishlistContainer.jsx
@@ -11,7 +11,7 @@ function WishlistContainer() {
         <div className="text-[2rem] mt-[1rem]">
           <h1 className="my-auto">Wishlists</h1>
         </div>
-        {wishList?.length !== 0 ? (
+        {wishList?.length !== 0 || !wishList ? (
           <div className="grid grid-cols-1 gap-10 mt-5 md:grid-cols-3">
             {wishList?.map((property) => (
               <WishlistItem key={property?.id} property={property} />
